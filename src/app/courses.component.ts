73e48ecf855d2005dc367cceb6ae53a15253429f
JanAbczynski <jan.abczynski@gmail.com>
2020-05-08 13:11:27 +0200

    .stopPropagation FAIL

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,43 +1,27 @@
 import { CoursesService } from './courses.service';
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core'
 
 @Component({
     selector: 'courses',
     template: `
-    <h2>{{ getTitle() }}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-            {{ course }}
-        </li>
-    </ul>
-    <img src="{{ imageUrl }}" />
-    <img [src]="imageUrl" />
-    <button class="btn btn-primary" [class.active]="isActive">button</button>
-    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">button2</button>
-    <table>
-        <tr>
-            <td [attr.colspan]="colSpan">xx</td>
-        </tr>
-    </table>
-    
-    `
+    <div (click)="onDivClick()">
+        <button (click)="onSave($event)">Save</button>
+    </div>
+        `
 })
+
 export class CoursesComponent {
-    isActive = false;
-    coalSpan = 2;
-    title = "List of courses";
-    imageUrl = "http://lorempixel.com/400/200";
-    courses;
 
-    constructor (service: CoursesService){
-        // let service = new CoursesService();
-        this.courses = service.getCourses();
-    
-    }
-
-    getTitle(){
-        return this.title;
+    onDivClick(){
+                console.log("Div click")
 
     }
+
+    onSave($event){
+        // @event.stopPropagation();
+        console.log('asd');
+    }
+
+
 
 }
