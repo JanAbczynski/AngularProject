3780d885353f6acb85fcd98b774d2b1180ddaacc
JanAbczynski <jan.abczynski@gmail.com>
2020-05-08 13:18:08 +0200

    on enter ver1

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,9 +4,12 @@ import { Component  } from '@angular/core'
 @Component({
     selector: 'courses',
     template: `
-    <div (click)="onDivClick()">
-        <button (click)="onSave($event)">Save</button>
-    </div>
+        <div (click)="onDivClick()">
+            <button (click)="onSave($event)">Save</button>
+        </div>
+        <div>
+            <input (keyup)="onKeyUp($event)" />
+        </div>
         `
 })
 
@@ -14,7 +17,10 @@ export class CoursesComponent {
 
     onDivClick(){
                 console.log("Div click")
+    }
 
+    onKeyUp($event){
+        if ($event.keyCode === 13) console.log("ENTER was pressed");
     }
 
     onSave($event){
