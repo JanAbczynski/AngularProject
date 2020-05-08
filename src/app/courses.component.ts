175d8067abbb2e346188159db9eb01c854d9d290
JanAbczynski <jan.abczynski@gmail.com>
2020-05-08 16:16:41 +0200

    own pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,23 +4,14 @@ import { Component  } from '@angular/core'
 @Component({
     selector: 'courses',
     template: `
-    {{ course.title | uppercase}} <br/>
-    {{ course.students | number}} <br/>
-    {{ course.rating | number:'2.2-2' }} <br/>
-    {{ course.price | currency:'PLN ':true:'3.2-2' }} <br/>
-    {{ course.releaseDate | date:'shortDate' }} <br/>
+    {{ text | summary:3 }} 
         `
 })
 
 export class CoursesComponent {
 
-    course = {
-        title: "The title",
-        rating: 4.1,
-        students: 101200,
-        price: 130.23,
-        releaseDate: new Date(2015, 12, 22)
-    }
+    text = `qwe asd zxc wer sdf xcv ert dfg cvb rty fgh vbn
+    `
 
 
 }
