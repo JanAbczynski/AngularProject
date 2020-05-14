c7e211568466e18d225ee48cc903454d3978cc8d
JanAbczynski <jan.abczynski@gmail.com>
2020-05-14 13:54:37 +0200

    validation with server FAIL

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,9 +1,54 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { resolve } from 'url';
 
 export class UsernameValidators {
     static cannotContainSpace(control: AbstractControl) : ValidationErrors | null{
         if ((control.value as string).indexOf(' ') >= 0)
             return { cannotContainSpace: true};
         return null;
+    } 
+
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null>{
+
+        let test: any;
+
+
+
+        // TA WERSJA DZIAŁA ALE NIE MA TIMEOUT
+
+        // test = new Promise((resolve, reject) => {
+        //        if (control.value === 'john')
+        //             resolve({shouldBeUnique : true});
+        //         else 
+        //             resolve(null); 
+
+        // });
+
+
+
+
+
+        // PO DODATNIU TIMEOUT NIE DZIAŁA JAK NALEŻY
+
+        test = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'john')
+                    resolve({shouldBeUnique : true});
+                else 
+                    resolve(null);       
+            }, 2000);
+
+        });
+
+
+
+
+
+        console.log(test.__zone_symbol__value)
+
+        return test.__zone_symbol__value;
     }
+
+
+
 }
